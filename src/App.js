b1b06c8934e2156d2cd23c9ee6ca979f368856b6
bojankovic95 <68285886+bojankovic95@users.noreply.github.com>
2023-11-21 20:33:08 +0100

    Init project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="h-screen">
+        <div className="p-5 m-0 bg-blue-600">
+            <h1 className="text-xl font-bold text-[red] underline">TailwindCSS Test</h1>
+        </div>
+        <div className="p-2 m-5 bg-cyan-300 h-full">
+            <p className="text-xl text-slate-500 font-bold text-center">Hello from TailwindCSS....!!!</p>
+        </div>  
+    </div>
+    </div>
+  );
+}
+
+export default App;
